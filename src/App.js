274d2a61bb850e2baea6f08b09786c511a80a8ea
saadSarwar28 <saad_sarwar22@hotmail.com>
2022-03-16 03:40:01 +0500

    repo updated with react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from './pages/MainPage';
+// import "./asset/sass/"
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
