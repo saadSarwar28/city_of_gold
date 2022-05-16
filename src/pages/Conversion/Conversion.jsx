7d4908e74c3055fa55417dd64969d73ffe468f24
saadSarwar28 <saad_sarwar22@hotmail.com>
2022-05-17 03:38:46 +0500

    contract addresses keys updated

--- src/pages/Conversion/Conversion.jsx
+++ src/pages/Conversion/Conversion.jsx
@@ -27,7 +27,7 @@ const Conversion = () => {
 
     useEffect(() => {
         if (window.ethereum && landContract !== null && address !== '') {
-            landContract.isApprovedForAll(address, Addresses.estate)
+            landContract.isApprovedForAll(address, Addresses.ESTATE)
                 .then(res => {
                     if (res) {
                         setApproved(true)
@@ -64,8 +64,8 @@ const Conversion = () => {
 
     useEffect(() => {
         if (window.ethereum && address !== '') {
-            setLandContract(new ethers.Contract(Addresses.land, landAbi, provider))
-            setEstateContract(new ethers.Contract(Addresses.estate, estateAbi, provider))
+            setLandContract(new ethers.Contract(Addresses.LAND, landAbi, provider))
+            setEstateContract(new ethers.Contract(Addresses.ESTATE, estateAbi, provider))
         }
     }, [address])
 
@@ -106,7 +106,7 @@ const Conversion = () => {
         }
         if (window.ethereum && address !== '') {
             const landContractSigned = landContract.connect(signer)
-            const tx = await landContractSigned.setApprovalForAll(Addresses.estate, true)
+            const tx = await landContractSigned.setApprovalForAll(Addresses.ESTATE, true)
             await tx.wait()
             setApproved(true)
             showWarningToast(successMessages.LAND_APPROVED)
